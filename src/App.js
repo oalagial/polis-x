3a48776f2e8ec9604281a7539683a88ebf262cd7
orestisal97 <orestisal97@gmail.com>
2022-02-20 15:17:01 +0200

    Make all appropriate changes for Hackathon Presentation of Demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import logo from './logo.svg';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Sidebar from './components/Sidebar';
-import PageContent from './components/PageContent';
-import Dashboard from './components/Dashboard';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Dashboard from "./components/LandingPage";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import Navbar from "./components/Navbar";
+import HealthProblemPage from "./components/HealthProblemPage";
 
 function App() {
   return (
     <div className="App">
       <div className="App">
-        <BrowserRouter>
-        <Dashboard/>
-          <Sidebar />
-          <PageContent />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/health-problem" element={<HealthProblemPage />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
       </div>
     </div>
   );
